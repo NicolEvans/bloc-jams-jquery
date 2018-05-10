fa89b63edd165def6079a2823d5b3657a3a11fbd
Nicole Evans <niclsevans@gmail.com>
2018-05-10 15:42:13 -0400

    icon url update and seconds reformat

--- scripts/player.js
+++ scripts/player.js
@@ -51,7 +51,7 @@ class Player {
   prettyTime(timeInSeconds) {
     const minutes = Math.floor(timeInSeconds / 60);
     const seconds = Math.floor(timeInSeconds % 60);
-    return minutes+":"+seconds;
+    return minutes+":"+((seconds < 10 ? '0' : '') + seconds);
 
   }
 }
